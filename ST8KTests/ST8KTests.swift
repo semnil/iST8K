24577a5c99f906252525096f0bb88884c585e70f
Tetsuya Shinone <info@semnil.com>
2016-08-08 22:10:34 +0900

    create a project.

---
+++ ST8KTests/ST8KTests.swift
@@ -0,0 +1,36 @@
+//
+//  ST8KTests.swift
+//  ST8KTests
+//
+//  Created by Tetsuya Shinone on 8/8/16.
+//  Copyright © 2016 semnil.com. All rights reserved.
+//
+
+import XCTest
+@testable import ST8K
+
+class ST8KTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
